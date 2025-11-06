45a98d87abb0b6c6886c83b1deb4fc0dd764340d
shaikh sufyan <shaikhsufyan1811@gmail.com>
2025-11-06 09:52:42 +0530

    Pagination in ReactJS

---
+++ pagination-react/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Pagination from './Components/Pagination';
+
+function App() {
+  return (
+    <Pagination/>
+  );
+}
+
+export default App;
