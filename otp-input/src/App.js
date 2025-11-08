e2463304115ac231afa02bc739b96a564f77dee6
shaikh sufyan <shaikhsufyan1811@gmail.com>
2025-11-08 11:25:47 +0530

    OTP Input Using ReactJS

---
+++ otp-input/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Otp from './Component/Otp';
+ 
+
+function App() {
+  return (
+    <>
+      <Otp/>
+    </>
+  );
+}
+
+export default App;
